987700f0243cc57142bdc152c275492f275d465e
Ahmed <ahmedehab1677@gmail.com>
2025-12-02 00:18:07 +0200

    setup convex and wrap whole app with its provider

--- src/app/(main)/navbar.tsx
+++ src/app/(main)/navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.webp";
 import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 import Link from "next/link";
