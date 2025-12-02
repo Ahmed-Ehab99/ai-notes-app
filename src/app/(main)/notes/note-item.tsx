f15b90095560b91f8187f63c26a89f5c623cede8
Ahmed <ahmedehab1677@gmail.com>
2025-12-03 01:13:09 +0200

    Implement open dialog on modal using window.history.pushState() for none navigation, refreshing or remount. this approach faster than router.push()

--- src/app/(main)/notes/note-item.tsx
+++ src/app/(main)/notes/note-item.tsx
@@ -1,14 +1,26 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { NotePreviewDialog } from "./note-preview-dialog";
 
 interface NoteItemProps {
   note: Doc<"notes">;
 }
 
 export function NoteItem({ note }: NoteItemProps) {
+  function handleOpenNode() {
+    // Update URL without navigating, without refreshing, and without causing React to remount.
+    // Useful because refreshing or sharing still opens that note
+    // Just to open a modal
+    // Also it is faster than router.push()
+    window.history.pushState(null, "", `?noteId=${note._id}`);
+  }
+
   return (
     <>
-      <Card className="cursor-pointer transition-shadow hover:shadow-md">
+      <Card
+        className="cursor-pointer transition-shadow hover:shadow-md"
+        onClick={handleOpenNode}
+      >
         <CardHeader>
           <CardTitle>{note.title}</CardTitle>
         </CardHeader>
@@ -18,6 +30,7 @@ export function NoteItem({ note }: NoteItemProps) {
           </div>
         </CardContent>
       </Card>
+      <NotePreviewDialog note={note} />
     </>
   );
 }
--- src/app/(main)/notes/note-preview-dialog.tsx
+++ src/app/(main)/notes/note-preview-dialog.tsx
@@ -4,20 +4,37 @@ import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
+  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Trash2 } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import { Doc } from "../../../../convex/_generated/dataModel";
 
-export function NotePreviewDialog() {
+interface NotePreviewDialogProps {
+  note: Doc<"notes">;
+}
+
+export function NotePreviewDialog({ note }: NotePreviewDialogProps) {
+  const searchParams = useSearchParams();
+  const isOpen = searchParams.get("noteId") === note._id;
+
+  function handleClose() {
+    window.history.pushState(null, "", window.location.pathname);
+  }
+
   return (
-    <Dialog>
+    <Dialog open={isOpen} onOpenChange={handleClose}>
       <DialogContent className="sm:max-w-[625px]">
         <DialogHeader>
-          <DialogTitle>Note Title</DialogTitle>
+          <DialogTitle>{note.title}</DialogTitle>
+          <DialogDescription className="sr-only">
+            Fill in your note details.
+          </DialogDescription>
         </DialogHeader>
-        <div className="mt-4 whitespace-pre-wrap">Note Body</div>
+        <div className="mt-4 whitespace-pre-wrap">{note.body}</div>
         <DialogFooter className="mt-6">
           <Button variant="destructive" className="gap-2">
             <Trash2 size={16} />
